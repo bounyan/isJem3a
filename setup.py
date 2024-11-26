d4bc8d191e91a03003caa538df2ecf67f02d5e21
Yannis Bounemer <bounemeryannis@gmail.com>
2024-11-26 11:20:27 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     name="isJem3a",
     version="1.0.0",
     description="A package to check if tomorrow is Friday",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     author="chkoupey",
+    author_email="bounemeryannis@gmail.com",
+    url="https://github.com/bounyan/isJem3a",
     packages=find_packages(),
     install_requires=[],
     entry_points={
